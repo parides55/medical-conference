971fa659bb676d02ae2877aedca120d817073b52
parides55 <andreasparides5@gmail.com>
2024-12-11 23:24:27 +0000

    add the registration.html to the registration button

--- home_page/views.py
+++ home_page/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'home_page/index.html')
+
+
+def registration(request):
+    return render(request, 'home_page/registration.html')
