86989c17bbb7ab486fa49ee3c5c3629f763744c3
parides55 <andreasparides5@gmail.com>
2024-12-15 01:09:48 +0000

    add try and except tags in the views of the conference app

--- conference/views.py
+++ conference/views.py
@@ -1,25 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Speaker
 
 # Create your views here.
 def conference_info(request):
-    return render(request, 'conference/conference_info.html')
+    try:
+        return render(request, 'conference/conference_info.html')
+
+    except Exception as e:
+        messages.error(request, f"The following error occurred: {str(e)}")
+        return redirect('conference_info')
 
 
 def conference_schedule(request):
-    return render(request, 'conference/conference_schedule.html')
+    try:
+        return render(request, 'conference/conference_schedule.html')
+
+    except Exception as e:
+        messages.error(request, f"The following error occurred: {str(e)}")
+        return redirect('conference_schedule')
 
 
 def speakers(request):
     
-    speakers = Speaker.objects.all()
-    
-    context = {
-        'speakers': speakers
-    }
-    
-    return render(request, 'conference/speakers.html', context)
+    try:
+        speakers = Speaker.objects.all()
+        
+        context = {
+            'speakers': speakers
+        }
+        
+        return render(request, 'conference/speakers.html', context)
+
+    except Speaker.DoesNotExist:
+        messages.info(request, 'News not found.')
+        return redirect('speakers')
+
+    except Exception as e:
+        messages.error(request, f"The following error occurred: {str(e)}")
+        return redirect('speakers')
+        
 
 
 def sponsors(request):
-    return render(request, 'conference/sponsors.html')
+    try:
+        return render(request, 'conference/sponsors.html')
+
+    except Exception as e:
+        messages.error(request, f"The following error occurred: {str(e)}")
+        return redirect('sponsors')
