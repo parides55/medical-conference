4a7b57d7070eedcd72b064c52cd06c4d57bff017
parides55 <andreasparides5@gmail.com>
2025-03-12 23:31:49 +0000

    add status field and remigrate

--- about_us/models.py
+++ about_us/models.py
@@ -3,9 +3,16 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class PresidentMessage(models.Model):
+    
+    STATUS = (
+        (1, 'president'),
+        (2, 'not president'),
+    )
+    
     title = models.CharField(max_length=100)
     message = models.TextField()
     image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=1)
     is_active = models.BooleanField(default=False)
 
     def __str__(self):
