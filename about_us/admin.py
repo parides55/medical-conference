7cf099c20bcfd85d6f7f403bd1baa69abef2b53a
parides55 <andreasparides5@gmail.com>
2025-03-12 21:56:33 +0000

    created a model for presidents message and add it in the admin panel

--- about_us/admin.py
+++ about_us/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import PresidentMessage
 
 # Register your models here.
+@admin.register(PresidentMessage)
+class PresidentMessageAdmin(SummernoteModelAdmin):
+
+    list_display = ('title')
+    summernote_fields = ('message',)
--- about_us/models.py
+++ about_us/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class PresidentMessage(models.Model):
+    title = models.CharField(max_length=100)
+    message = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.title
