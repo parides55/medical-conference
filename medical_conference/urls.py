08fe21e7f2d9360bdddcad6f6da861d06320db74
parides55 <andreasparides5@gmail.com>
2025-03-12 22:02:39 +0000

    install django-summernote, add to installed apps and update the requirements.txt

--- medical_conference/urls.py
+++ medical_conference/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('home_page.urls'), name='home_page'),
     path('about_us', include('about_us.urls'), name='about_us'),
     path('contact_us', include('contact_us.urls'), name='contact_us'),
