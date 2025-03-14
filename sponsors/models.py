7a3b58cce63977a4bf419a343c57683e35ce8436
parides55 <andreasparides5@gmail.com>
2025-03-14 12:49:38 +0000

    create a sponsors model, migrate it and add it to the admin panel

--- sponsors/models.py
+++ sponsors/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Sponsor(models.Model):
+    
+    STATUS = (
+        ('Platinum', 'Platinum'),
+        ('Gold', 'Gold'),
+        ('Silver', 'Silver'),
+        ('Other', 'Other'),
+    )
+    
+    name = models.CharField(max_length=200)
+    logo = CloudinaryField('image', default='placeholder')
+    url = models.URLField(blank=True)
+    status = models.IntegerField(choices=STATUS, default='Other')
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.is_active}'
