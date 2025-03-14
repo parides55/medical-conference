933d10023a2738b819ac8287a8560f464a7b5469
parides55 <andreasparides5@gmail.com>
2025-03-14 13:47:17 +0000

    remove print() for debugging

--- sponsors/views.py
+++ sponsors/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .models import Sponsor
 
 # Create your views here.
 def sponsors(request):
     try:
-        return render(request, 'sponsors/sponsors.html')
+        sponsors = Sponsor.objects.filter(is_active=True)
+
+        context = {
+            'sponsors': sponsors,
+        }
+        
+        return render(request, 'sponsors/sponsors.html', context)
 
     except Exception as e:
         messages.error(request, f"The following error occurred: {str(e)}")
