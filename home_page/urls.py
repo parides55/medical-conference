44e8130a22e9dc12f367859dbffebf6332547d8d
parides55 <andreasparides5@gmail.com>
2025-03-14 14:41:00 +0000

    delete and remove the registration.html

--- home_page/urls.py
+++ home_page/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('registration/', views.registration, name='registration'),
 ]
--- home_page/views.py
+++ home_page/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'home_page/index.html')
-
-
-def registration(request):
-    return render(request, 'home_page/registration.html')
